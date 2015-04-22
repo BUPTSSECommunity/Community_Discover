24215da1975a362d48546b94669658e38b52c84d
chenyufei <chenyufei_fly@163.com>
2015-04-22 17:38:25 +0800

    完善了Node类

--- src/algorithm/Algorithm_SeedNode.java
+++ src/algorithm/Algorithm_SeedNode.java
@@ -1,7 +1,9 @@
 package algorithm;
 
+import data.Node;
+
 public class Algorithm_SeedNode {
-public boolean execute(){
+public boolean execute(Node[] nodes_array/*in/out*/, int[][] link_matrix/*in*/,String[] seed_label/*in*/, int[] seed_node/*out*/){
 		
 		return true;
 	}
--- src/data/Node.java
+++ src/data/Node.java
@@ -1,5 +1,26 @@
 package data;
 
+import java.util.List;
+
 public class Node {
   private static final String TAG = "Node";
+  
+  public int id;
+  public String username;
+  public List<String> label_list;
+  public String label;
+  
+  //add a label to this node , stored in label_list
+  public boolean addLabel(String newLabel)
+  {
+	  label_list.add(newLabel);
+	  return true;
+  }
+  
+  //free all the labels in label_list
+  public boolean freeAllLabel(){
+	  label_list.clear();
+	  return true;
+  }
+  
 }
