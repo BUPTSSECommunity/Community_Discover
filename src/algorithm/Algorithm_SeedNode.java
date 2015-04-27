fda4cc277966cde21d35c7fcb2efb69d64fa2c20
chenyufei <chenyufei_fly@163.com>
2015-04-27 08:49:30 +0800

    稍微改了Node类，种子节点算法写了一半

--- src/algorithm/Algorithm_SeedNode.java
+++ src/algorithm/Algorithm_SeedNode.java
@@ -1,10 +1,60 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import data.Node;
 
 public class Algorithm_SeedNode {
-public boolean execute(Node[] nodes_array/*in/out*/, int[][] link_matrix/*in*/,String[] seed_label/*in*/, int[] seed_node/*out*/){
+	/**
+	 * 求解种子节点，并向种子节点中加入对应的种子标签
+	 * @param nodes_array 执行前：所有节点列表；执行后：列表中的种子节点已包含种子标签
+	 * @param link_matrix 节点关联矩阵
+	 * @param seed_label 种子标签名称列表
+	 * @param x 平滑因子，暂时建议大于0.5；选取种子节点时，x越大则节点度数越重要，x越小则种子标签在原始标签中出现的次数越重要
+	 * @return 种子节点在 nodes_array 中的对应下标列表
+	 */
+	public int[] execute(Node[] nodes_array/*in/out*/, int[][] link_matrix/*in*/,String[] seed_label/*in*/,double x){
+		int K = seed_label.length;//种子标签数量
+		int[] seed_node = new int[K];//最终需要返回的种子节点列表
+		int nodeSize = link_matrix.length;//节点的数量
 		
-		return true;
+		for(int i=0;i<K;i++){
+			double[] suit = new double[nodeSize];//为所有节点建立针对第i个种子标签的suit值列表
+			int maxSuitIndex = 0;
+			//遍历所有节点，针对每个节点j计算suit值
+			for(int j=0;j<nodeSize;j++){
+				double degree = (double)countDegree(j,link_matrix);//得到节点j的度
+				double appear = (double)nodes_array[j].label_list.get(seed_label[i]);//标签i在节点j的原始标签中出现的次数
+				
+				suit[j] = degree*x + appear*(1-x);//计算节点j的suit
+				//记录最大suit值的下标
+				if(suit[j] >= suit[maxSuitIndex]){
+					maxSuitIndex = j;
+				}
+			}
+			
+			//对于下标为 maxSuitIndex 的节点，清空标签列表，赋予种子标签i
+		}
+		
+		return seed_node;
+	}
+	
+	/**
+	 * 计算指定节点的度
+	 * @param nodeIndex 指定节点在矩阵的下标
+	 * @param link_matrix 关联矩阵
+	 * @return 该节点的度
+	 */
+	private int countDegree(int nodeIndex, int[][] link_matrix){
+		int result = 0;//所求节点的度
+		int nodeSize = link_matrix.length;
+		//遍历矩阵的第nodeIndex行，计算1的数量
+		for(int i=0;i<nodeSize;i++){
+			if(link_matrix[nodeIndex][i] == 1){
+				result++;
+			}
+		}
+		return result;
 	}
 }
--- src/data/Node.java
+++ src/data/Node.java
@@ -1,23 +1,43 @@
 package data;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class Node {
   private static final String TAG = "Node";
   
+  /**该节点在关联矩阵中的对应下标*/
   public int id;
+  /**该节点代表的微博用户名称*/
   public String username;
-  public List<String> label_list;
+  /**
+   * 种子节点求解前：该节点的原始标签列表；
+   * 种子节点求解后：该节点被打上的标签列表
+   */
+  public HashMap<String,Integer> label_list = new HashMap<String, Integer>();
+  /**该节点初始标签*/
   public String label;
   
-  //add a label to this node , stored in label_list
+  /**
+   * 向标签列表中添加一个标签，如果该标签存在，则标签计数加1
+   * @param newLabel 标签名称
+   * @return 暂定为true
+   */
   public boolean addLabel(String newLabel)
   {
-	  label_list.add(newLabel);
+	  Integer count = label_list.get(newLabel);
+	  if(count == null){
+		  label_list.put(newLabel, 1);
+	  }else{
+		  label_list.put(newLabel, count+1);
+	  }
+	  
 	  return true;
   }
   
-  //free all the labels in label_list
+  /**
+   * 清空标签列表的所有标签
+   * @return 暂定为true
+   */
   public boolean freeAllLabel(){
 	  label_list.clear();
 	  return true;
