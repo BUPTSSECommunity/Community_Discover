c0a836fff5b589a580978ec2f32f04e266bc90fc
chenyufei <chenyufei_fly@163.com>
2015-05-02 17:30:57 +0800

    添加了gitignore文件，忽略src/test目录；按照新的数据结构完成了种子节点算法

--- src/algorithm/Algorithm_SeedNode.java
+++ src/algorithm/Algorithm_SeedNode.java
@@ -14,27 +14,38 @@ public class Algorithm_SeedNode {
 	 * @param x 平滑因子，暂时建议大于0.5；选取种子节点时，x越大则节点度数越重要，x越小则种子标签在原始标签中出现的次数越重要
 	 * @return 种子节点在 nodes_array 中的对应下标列表
 	 */
-	public int[] execute(Node[] nodes_array/*in/out*/, int[][] link_matrix/*in*/,String[] seed_label/*in*/,double x){
+	public List<Integer> execute(Node[] nodes_array/*in/out*/, int[][] link_matrix/*in*/,String[] seed_label/*in*/,double x){
 		int K = seed_label.length;//种子标签数量
-		int[] seed_node = new int[K];//最终需要返回的种子节点列表
+		List<Integer> seed_node = new ArrayList<Integer>();//最终需要返回的种子节点列表
 		int nodeSize = link_matrix.length;//节点的数量
 		
+		//对于每个种子标签，计算suit最大的节点
 		for(int i=0;i<K;i++){
 			double[] suit = new double[nodeSize];//为所有节点建立针对第i个种子标签的suit值列表
 			int maxSuitIndex = 0;
 			//遍历所有节点，针对每个节点j计算suit值
 			for(int j=0;j<nodeSize;j++){
-				double degree = (double)countDegree(j,link_matrix);//得到节点j的度
+				double degree = (double)nodes_array[j].degree;//得到节点j的度
 				double appear = (double)nodes_array[j].label_list.get(seed_label[i]);//标签i在节点j的原始标签中出现的次数
 				
 				suit[j] = degree*x + appear*(1-x);//计算节点j的suit
 				//记录最大suit值的下标
-				if(suit[j] >= suit[maxSuitIndex]){
+				if(suit[j] >= suit[maxSuitIndex]){//当前允许同一个节点被选为多个标签的种子，如果需要改为种子节点不重叠，则添加判断该节点是否在seed_node列表中
 					maxSuitIndex = j;
 				}
 			}
 			
 			//对于下标为 maxSuitIndex 的节点，清空标签列表，赋予种子标签i
+			nodes_array[maxSuitIndex].label_list.clear();
+			nodes_array[maxSuitIndex].addLabel(seed_label[i]);
+			seed_node.add(maxSuitIndex);//将该节点下标记入种子节点列表
+		}
+		
+		//清空除种子节点之外所有节点的标签列表
+		for(int j=0;j<nodeSize;j++){
+			if(!seed_node.contains(j)){//如果下标为j的点不是种子节点
+				nodes_array[j].freeAllLabel();//清空标签
+			}
 		}
 		
 		return seed_node;
